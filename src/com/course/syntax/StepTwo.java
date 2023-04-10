73e04a43bb3fe65b3a24c2dcaf604327fe9e4699
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-10 20:16:51 +0300

    1 Task / Operations

--- src/com/course/syntax/StepTwo.java
+++ src/com/course/syntax/StepTwo.java
@@ -3,4 +3,16 @@ package com.course.syntax;
 import java.math.MathContext;
 
 public class StepTwo {
+    public static void main(String[] args) {
+        int a = 14;
+        int b = 12;
+        int c = 12;
+        int p = (a + b + c)/2;
+
+        double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        System.out.println(S);
+
+}
+}
 
