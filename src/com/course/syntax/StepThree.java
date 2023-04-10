4592e00a02d8108774f977ce84c714f0b02525fc
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-10 10:10:24 +0300

    Task 2 / Conditions

--- src/com/course/syntax/StepThree.java
+++ src/com/course/syntax/StepThree.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class StepThree {
     public static void main(String[] args) {
-        System.out.print("Введите любое целое число от 1 до 10: ");
+    /*    System.out.print("Введите любое целое число от 1 до 10: ");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
 
@@ -18,7 +18,21 @@ public class StepThree {
             System.out.println("This number " + number + " is not that good as a previous one");
         } else {
             System.out.println("You've entered " + number);
-        }
+        } */
+
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter a double value: ");
+                double num = scanner.nextDouble();
+
+                if (num % 2 == 0) {
+                    System.out.println(num + " is even.");
+                } else {
+                    System.out.println(num + " is odd.");
+                }
+
+                scanner.close();
+
     }
 
     }
