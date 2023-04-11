f82ed75f4d3f4556dfa241e7fc228a654526d535
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-11 18:36:26 +0300

    Task 2 / Loops

--- src/com/course/syntax/StepFour.java
+++ src/com/course/syntax/StepFour.java
@@ -1,4 +1,22 @@
 package com.course.syntax;
 
+import java.util.Scanner;
+
 public class StepFour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a value to get factorial: ");
+        int n = scanner.nextInt();
+        System.out.println(Factorial(n));
+    }
+    public static int Factorial(int n) {
+        int m = 1;
+        for (int i = 1; i <= n; ++i) {
+            m *= i;
+        }
+        return m;
+    }
 }
+
+
