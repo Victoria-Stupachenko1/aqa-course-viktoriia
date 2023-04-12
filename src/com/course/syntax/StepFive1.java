ce6ed987e1110a1962dea45993f6dc1bf92d20a5
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-12 20:32:59 +0300

    Task 1 / Array

--- src/com/course/syntax/StepFive1.java
+++ src/com/course/syntax/StepFive1.java
@@ -2,15 +2,14 @@ package com.course.syntax;
 
 public class StepFive1 {
     public static void main(String[] args) {
-        int[] arr = {5, 10, 15, 20, 25};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         int sum = 0;
-        double average;
-
-        for(int num : arr) {
-            sum += num;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
         }
-
-        average = (double) sum / arr.length;
-        System.out.println("Average value of the array is: " + average);
+        double average = (double) sum / array.length;
+        System.out.println("Average length of my array is " + average);
     }
 }
+
+
