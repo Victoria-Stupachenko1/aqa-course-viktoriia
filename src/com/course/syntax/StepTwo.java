2dcecbf8c51bd0c587dcffc4d27ec3282655980e
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-12 20:16:49 +0300

    git commit

--- src/com/course/syntax/StepTwo.java
+++ src/com/course/syntax/StepTwo.java
@@ -2,7 +2,6 @@ package com.course.syntax;
 
 public class StepTwo {
     public static void main(String[] args) {
- variables
         int a = 14;
         int b = 12;
         int c = 12;
@@ -12,31 +11,5 @@ public class StepTwo {
 
         System.out.println(s);
 
-}
-
-        int a = 2;
-        int b = 3;
-        int c = 4;
-        int d = 6;
-
-        int e = 2 * a++;
-        int f = 2 * ++b;
-        int g = 2 * c--;
-        int h = 2 * --d;
-
-        System.out.println("Постфиксная форма инкремента " + e);
-        System.out.println(a);
-        System.out.println("Префиксная форма инкремента " + f);
-        System.out.println(b);
-        System.out.println("Постфиксная форма декремента " + g);
-        System.out.println(c);
-        System.out.println("Префиксная форма декремента " + h);
-        System.out.println(d);
-
-
-
-    }
-
- main
 }
 
