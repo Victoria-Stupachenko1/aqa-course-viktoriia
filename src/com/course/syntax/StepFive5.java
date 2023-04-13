758d28a6d2b2d807ad5b7ac91aae06dc9458a03c
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-13 17:35:34 +0300

    Task 5 / Arrays

---
+++ src/com/course/syntax/StepFive5.java
@@ -0,0 +1,26 @@
+package com.course.syntax;
+
+import java.util.Arrays;
+
+public class StepFive5 {
+    public static void main(String[] args) {
+        int [] bubbles = {12, 4, 15, 0, 8, 1};
+
+        boolean isSorted = false;
+        int method;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < bubbles.length-1; i++) {
+                if (bubbles[i] > bubbles[i+1]) {
+                    isSorted = false;
+
+                    method = bubbles[i];
+                    bubbles[i] = bubbles[i+1];
+                    bubbles[i+1] = method;
+
+                }
+            }
+        }
+        System.out.println(Arrays.toString(bubbles));
+    }
+}
