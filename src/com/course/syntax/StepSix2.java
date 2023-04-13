26dd62666e8ee77b990fca4b0315d937caae97e6
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-13 20:06:57 +0300

    Task 2 / String

--- src/com/course/syntax/StepSix2.java
+++ src/com/course/syntax/StepSix2.java
@@ -1,4 +1,18 @@
 package com.course.syntax;
 
 public class StepSix2 {
+    public static void main(String[] args) {
+        String sentence = "I like Java, but it is complicated sometimes";
+        String reverse = new StringBuffer(sentence).reverse().toString();
+
+         if (sentence.contains("Java")) {
+
+             System.out.println(reverse);
+         } else {
+             System.out.println(sentence.toUpperCase());
+         }
+
+
+    }
 }
+
