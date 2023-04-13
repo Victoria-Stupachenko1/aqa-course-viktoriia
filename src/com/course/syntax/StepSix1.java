a45277a51e904256b877294bed8669bb8440f30b
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-13 18:51:20 +0300

    Task 1 / String

--- src/com/course/syntax/StepSix1.java
+++ src/com/course/syntax/StepSix1.java
@@ -1,4 +1,14 @@
 package com.course.syntax;
 
+import java.util.Scanner;
+
 public class StepSix1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter the string:");
+        String string1 = scanner.nextLine();
+
+        System.out.println(string1.toUpperCase().replaceAll("\\s", "" ));
+
+    }
 }
