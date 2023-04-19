bf275171cb050f420c9cfabbb76672610ff192d7
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-19 20:08:53 +0300

    Step Five Task 2

--- src/com/course/syntax/StepFive2.java
+++ src/com/course/syntax/StepFive2.java
@@ -2,6 +2,21 @@ package com.course.syntax;
 
 public class StepFive2 {
     public static void main(String[] args) {
+        int [] array1 = {1, 2, 3, 4, 5, 6};
+        int [] array2 = {7, 8, 9, 10, 11, 12};
+        int [] arraySum = new int [array1.length + array2.length];
+        int count = 0;
+
+        for (int i = 0; i < array1.length; i++) {
+            arraySum[i] = array1[i];
+            count++;
+        }
+        for (int j = 0; j < array2.length; j++) {
+            arraySum[count++] = array2[j];
+        }
+        for (int i = 0; i < arraySum.length; i++) {
+            System.out.println(arraySum[i] + " ");
+        }
 
 
     }
