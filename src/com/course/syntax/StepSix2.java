c2a1cc5a8ef411177c7ece79e4be52862d750dd0
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-04-19 20:40:01 +0300

    Second word upper case

--- src/com/course/syntax/StepSix2.java
+++ src/com/course/syntax/StepSix2.java
@@ -3,16 +3,22 @@ package com.course.syntax;
 public class StepSix2 {
     public static void main(String[] args) {
         String sentence = "I like Java, but it is complicated sometimes";
-        String reverse = new StringBuffer(sentence).reverse().toString();
+        if (sentence.toLowerCase().contains("java")) {
 
-         if (sentence.contains("Java")) {
+            String reversedSentence = new StringBuilder(sentence).reverse().toString();
+            System.out.println(reversedSentence);
+        } else {
 
-             System.out.println(reverse);
-         } else {
-             System.out.println(sentence.toUpperCase());
-         }
+            String[] second = sentence.split(" ");
+            for (int i = 0; i < second.length; i++) {
+                if (i % 2 == 1) {
+                    System.out.print(second[i].toUpperCase() + " ");
+                } else {
+                    System.out.print(second[i] + " ");
 
 
+                }
+            }
+        }
     }
 }
-
